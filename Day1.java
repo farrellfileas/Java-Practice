a8c8bbfaaac3edebdd78d4a6759b96506fc9c14a
gaaronah <gaaronah@users.noreply.github.com>
2020-12-09 23:11:17 -0800

    Update Day1.java

--- Day1.java
+++ Day1.java
@@ -3,11 +3,11 @@ import java.util.*;
 public class Day1 {
     public static void main(String[] args) {
         List<List<Integer>> wall = transform(new int[][]{{1,2,2,1},
-                        {3,1,2},
-                        {1,3,2},
-                        {2,4},
-                        {3,1,2},
-                        {1,3,1,1}});
+                                                        {3,1,2},
+                                                        {1,3,2},
+                                                        {2,4},  
+                                                        {3,1,2},
+                                                        {1,3,1,1}});
         
         int result = leastBricks(wall);
         
@@ -20,7 +20,22 @@ public class Day1 {
      * @return the least amount of bricks needed to be removed to pass the wall
      */
     private static int leastBricks(List<List<Integer>> wall) {
-        return 0;
+        HashMap<Integer, Integer> totalOccurance = new HashMap<>();
+        int total = 0;
+        for (int i = 0; i < wall.size(); i++) {
+            int tempTotal = 0;
+            for (int j = 0; j < wall.get(i).size() - 1; j++) {
+                tempTotal += wall.get(i).get(j);
+                if (totalOccurance.containsKey(tempTotal)) {
+                    totalOccurance.put(tempTotal, totalOccurance.get(tempTotal) + 1);
+                }
+                else {
+                    totalOccurance.put(tempTotal, 1);
+                }
+                total = Math.max(total, totalOccurance.get(tempTotal));
+            }
+        }
+        return wall.size() - total;
     }
     
     private static List<List<Integer>> transform(int[][] wall) {
