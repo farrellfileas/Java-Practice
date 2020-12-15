090856e682c0b3ad543f9d64ac0245e1bb17f5cd
gaaronah <gaaronah@users.noreply.github.com>
2020-12-14 17:31:36 -0800

    day 8 and day 9

--- Day8.java
+++ Day8.java
@@ -1,13 +1,99 @@
+import java.util.*;
+
 public class Day8 {
     public static void main(String[] agrs) {
         int[] arr = {3, 2, 1, 5, 6, 4};
-        System.out.println(findSmallest(arr)); // Expected 1 
+        System.out.println(findSmallest(arr,4)); // Expected 1 
 
         arr = new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6};
-        System.out.println(findSmallest(arr)); // Expected 1
+        System.out.println(findSmallest(arr,3)); // Expected 1
+
+        buildHeap(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
-    private static int findSmallest(int[] arr) {
-        return 0;
+    private static List<Integer> findSmallest(int[] arr , int k) {
+        /*
+        int smallest = Integer.MAX_VALUE;
+        int secondSmallest = Integer.MAX_VALUE;
+        int index = Integer.MAX_VALUE;
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (smallest >= arr[i]) {
+                index = i;
+            }
+            smallest = Math.min(arr[i], smallest);
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (i != index) {
+                secondSmallest = Math.min(arr[i], secondSmallest);
+            }
+        }
+        result.add(smallest);
+        result.add(secondSmallest);
+        */
+        List<Integer> result = new ArrayList<>();
+        buildHeap(arr);
+        for (int i = 0; i < k; i++) {
+            result.add(popTop(arr, i));
+        }
+        return result;
+    }
+
+//============================ Min Heap =============================
+
+    private static void swap(int a, int b, int[] arr) {
+        int temp = arr[b];
+        arr[b] = arr[a];
+        arr[a] = temp;
+    }
+
+    private static void percolateDown(int[] arr, int index) {
+        while(2 * index + 1 < arr.length) {
+            int leftChild = 2 * index + 1;
+            int rightChild = 2 * index + 2;
+            int min = Math.min(arr[index], arr[leftChild]);
+            if(rightChild < arr.length) {
+                min = Math.min(min, arr[rightChild]);
+            }
+
+            if (min == arr[index]) {
+                break;
+            }
+            else if (min == arr[leftChild]) {
+                swap(leftChild, index, arr);
+                index = leftChild;
+            }
+            else {
+                swap(rightChild, index, arr);
+                index = rightChild;
+            }
+        }
+    }
+
+    private static void buildHeap(int[] arr) {
+        for (int i = arr.length - 1; i >=0 ; i--) {
+            percolateDown(arr, i);
+        }
+    }
+
+    private static int popTop(int[] arr, int i) {
+        int result = arr[0];
+        arr[0] = arr[arr.length - 1 - i];
+        percolateDown(arr, 0);
+        return result;
     }
 }
+
+/*
+        1
+      /    \
+    2       4
+   / 
+  3 
+
+  N 1 2 4 3
+*/
+
+
--- Day9.java
+++ Day9.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Day9 {
     public static void main(String[] args) {
         // ((2 + 1) * 3) = 9
@@ -5,6 +7,8 @@ public class Day9 {
 
         // (4 + (13 / 5)) = 6
         System.out.println(evalRPN(new String[]{"4", "13", "5", "/", "+"}));
+        // []
+        // [6] 4 + 2
 
         //   ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
         // = ((10 * (6 / (12 * -11))) + 17) + 5
@@ -17,6 +21,32 @@ public class Day9 {
     }
 
     private static int evalRPN(String[] tokens) {
-        return 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < tokens.length; i++) {
+            if (!tokens[i].equals("+") && !tokens[i].equals("-") && !tokens[i].equals("/") && !tokens[i].equals("*")) {
+                int number = Integer.parseInt(tokens[i]);
+                stack.push(number);
+            }
+            else {
+                int right = stack.pop();
+                int left = stack.pop();
+                int total = 0;
+
+                if(tokens[i].equals("+")) {
+                    total += right + left;
+                }
+                if(tokens[i].equals("-")) {
+                    total += left - right;
+                }
+                if(tokens[i].equals("*")) {
+                    total += right * left;
+                }
+                if(tokens[i].equals("/")) {
+                    total += left / right;
+                }
+                stack.push(total);
+            }
+        }
+        return stack.get(0);
     }
 }
